6b78e4d2ca8523274417d3fa15619ecc97b5adca
mohameddsalah60 <mohamedsalahdev2004@gmail.com>
2025-11-25 22:36:13 +0200

    fix: Add missing type annotation for title parameter and enhance alert dialog styling

--- lib/core/widgets/custom_alert_dialog.dart
+++ lib/core/widgets/custom_alert_dialog.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void customAlertDialog(
   BuildContext context,
   String text,
-  title,
+  String title,
   Color color,
   VoidCallback? onPressed,
 ) {
@@ -11,17 +12,45 @@ void customAlertDialog(
     context: context,
     builder:
         (context) => AlertDialog(
-          title: Text(title),
-          content: Text(text),
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.r),
+          ),
+          title: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.sp),
+          ),
+          content: Text(text, style: TextStyle(fontSize: 16.sp)),
           actions: [
             TextButton(
-              child: const Text('إلغاء'),
+              child: const Text(
+                'إلغاء',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onPressed: () => Navigator.of(context).pop(),
             ),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: color),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: color,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.r),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+              ),
               onPressed: onPressed,
-              child: const Text('تأكيد', style: TextStyle(color: Colors.white)),
+              child: const Text(
+                'تأكيد',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
